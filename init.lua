737cf1e1ef97957f7dcb378632421d110cfb8070
Max Celant <maxcelant@gmail.com>
2026-02-07 22:27:10 -0600

    feat: default to gruvbox, fixing treesitter bug, filesystem defaults

--- init.lua
+++ init.lua
@@ -298,9 +298,13 @@ require('lazy').setup({
         --   },
         -- },
         defaults = {
-          file_ignore_patterns = { '^vendor/' },
+          file_ignore_patterns = { '^vendor/', '%.git/' },
         },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -790,18 +794,19 @@ require('lazy').setup({
     end,
   },
   { -- You can easily change to a different colorscheme.
-    'xiantang/darcula-dark.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('darcula').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require('gruvbox').setup {
+        italic = {
+          strings = false,
+          comments = false,
+          operators = false,
         },
       }
 
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'darcula-dark'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
   {
@@ -818,7 +823,13 @@ require('lazy').setup({
       { '<C-n>', '<cmd>Neotree toggle<CR>', desc = 'Toggle Neo-tree' },
     },
     opts = {
-      -- your Neo-tree config here
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
     },
   },
   {
@@ -924,21 +935,22 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function()
+      require('nvim-treesitter').setup {
+        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+          enable = true,
+          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          --  If you are experiencing weird indenting issues, add the language to
+          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+      }
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
